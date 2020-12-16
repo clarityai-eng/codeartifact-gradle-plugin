2dde3e93bdbda1eccc6bb07050e8486afb073b29
Joan Fisbein <joan.fisbein@clarity.ai>
2020-12-16 17:51:30 +0100

    Add URIBuilder

---
+++ src/main/groovy/ai/clarity/codeartifact/URIBuilder.java
@@ -0,0 +1,164 @@
+package ai.clarity.codeartifact;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class URIBuilder {
+
+  private String scheme;
+  private String fragment;
+  private String authority;
+  private String userInfo;
+  private String host;
+  private int port = -1;
+  private String path;
+  private Map<String, String> queryParams = new LinkedHashMap<>();
+
+  private URIBuilder() {
+
+  }
+
+  public static URIBuilder of(URI uri) {
+    return new URIBuilder()
+      .withScheme(uri.getScheme())
+      .withFragment(uri.getFragment())
+      .withAuthority(uri.getAuthority())
+      .withUserInfo(uri.getUserInfo())
+      .withHost(uri.getHost())
+      .withPort(uri.getPort())
+      .withPath(uri.getPath())
+      .withQuery(uri.getQuery());
+  }
+
+  public URIBuilder withScheme(String scheme) {
+    this.scheme = scheme;
+    return this;
+  }
+
+  public URIBuilder withFragment(String fragment) {
+    this.fragment = fragment;
+    return this;
+  }
+
+  public URIBuilder withAuthority(String authority) {
+    this.authority = authority;
+    return this;
+  }
+
+  public URIBuilder withUserInfo(String userInfo) {
+    this.userInfo = userInfo;
+    return this;
+  }
+
+  public URIBuilder withHost(String host) {
+    this.host = host;
+    return this;
+  }
+
+  public URIBuilder withPort(int port) {
+    this.port = port;
+    return this;
+  }
+
+  public URIBuilder withPath(String path) {
+    this.path = path;
+    return this;
+  }
+
+  public URIBuilder withQuery(String query) {
+    queryParams = parseQueryParams(query);
+    return this;
+  }
+
+  public String getQueryParamValue(String param) {
+    return queryParams.get(param);
+  }
+
+  public URIBuilder removeQueryParam(String param) {
+    queryParams.remove(param);
+    return this;
+  }
+
+  public URIBuilder setQueryParam(String param, String value) {
+    queryParams.put(param, value);
+    return this;
+  }
+
+  public URI toURI() throws URISyntaxException {
+    String query = generateQueryByParams();
+    return new URI(scheme, userInfo, host, port, path, query, fragment);
+  }
+
+  private String generateQueryByParams() {
+    String query = queryParams.entrySet().stream()
+      .map(e -> e.getKey() + "=" + e.getValue())
+      .collect(Collectors.joining("&"));
+
+    if (query.isEmpty()) {
+      query = null;
+    }
+
+    return query;
+  }
+
+  private Map<String, String> parseQueryParams(String query) {
+    Map<String, String> params = new LinkedHashMap<>();
+    if (query != null) {
+      if (query.startsWith("?")) {
+        query = query.substring(1);
+      }
+      String[] split = query.split("&");
+      for (String nameValue : split) {
+        String name;
+        String value;
+        int equalsIndex = nameValue.indexOf('=');
+        if (equalsIndex > 0) {
+          name = nameValue.substring(0, equalsIndex);
+          value = nameValue.substring(equalsIndex + 1);
+        } else {
+          name = nameValue;
+          value = null;
+        }
+
+        params.put(name, value);
+      }
+    }
+
+    return params;
+  }
+
+  public String getScheme() {
+    return scheme;
+  }
+
+  public String getFragment() {
+    return fragment;
+  }
+
+  public String getAuthority() {
+    return authority;
+  }
+
+  public String getUserInfo() {
+    return userInfo;
+  }
+
+  public String getHost() {
+    return host;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public String getQuery() {
+    return generateQueryByParams();
+  }
+}
---
+++ src/test/groovy/ai/clarity/codeartifact/URIBuilderTest.java
@@ -0,0 +1,83 @@
+package ai.clarity.codeartifact;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.junit.jupiter.api.Test;
+
+class URIBuilderTest {
+
+  @Test
+  void testBasic() throws URISyntaxException {
+    URI uri = URI.create("http://www.elpais.es");
+    URIBuilder builder = URIBuilder.of(uri);
+
+    assertThat(builder.getScheme()).isEqualTo("http");
+    assertThat(builder.getHost()).isEqualTo("www.elpais.es");
+    assertThat(builder.toURI()).isEqualTo(uri);
+  }
+
+  @Test
+  void testGetParam() throws URISyntaxException {
+    URI uri = URI.create("http://www.elpais.es/noticias/nacional?pais=francia&foo=bar");
+    URIBuilder builder = URIBuilder.of(uri);
+
+    assertThat(builder.getScheme()).isEqualTo("http");
+    assertThat(builder.getHost()).isEqualTo("www.elpais.es");
+    assertThat(builder.getQueryParamValue("pais")).isEqualTo("francia");
+    assertThat(builder.toURI()).isEqualTo(uri);
+  }
+
+  @Test
+  void testModifyParam() throws URISyntaxException {
+    URI uri = URI.create("http://www.elpais.es/noticias/nacional?pais=francia&foo=bar");
+    URIBuilder builder = URIBuilder.of(uri);
+    builder.setQueryParam("pais", "italia");
+
+    assertThat(builder.getScheme()).isEqualTo("http");
+    assertThat(builder.getHost()).isEqualTo("www.elpais.es");
+    assertThat(builder.getQueryParamValue("pais")).isEqualTo("italia");
+    assertThat(builder.toURI()).isEqualTo(URI.create("http://www.elpais.es/noticias/nacional?pais=italia&foo=bar"));
+  }
+
+  @Test
+  void testRemoveParam() throws URISyntaxException {
+    URI uri = URI.create("http://www.elpais.es/noticias/nacional?pais=francia&foo=bar");
+    URIBuilder builder = URIBuilder.of(uri);
+    builder.removeQueryParam("pais");
+
+    assertThat(builder.getScheme()).isEqualTo("http");
+    assertThat(builder.getHost()).isEqualTo("www.elpais.es");
+    assertThat(builder.getQueryParamValue("pais")).isNull();
+    assertThat(builder.toURI()).isEqualTo(URI.create("http://www.elpais.es/noticias/nacional?foo=bar"));
+  }
+
+  @Test
+  void testRemoveAllParams() throws URISyntaxException {
+    URI uri = URI.create("http://www.elpais.es/noticias/nacional?pais=francia&foo=bar");
+    URIBuilder builder = URIBuilder.of(uri);
+    builder.withQuery(null);
+
+    assertThat(builder.getScheme()).isEqualTo("http");
+    assertThat(builder.getHost()).isEqualTo("www.elpais.es");
+    assertThat(builder.getQueryParamValue("pais")).isNull();
+    assertThat(builder.toURI()).isEqualTo(URI.create("http://www.elpais.es/noticias/nacional"));
+  }
+
+  @Test
+  void testAllFields() throws URISyntaxException {
+    URI uri = URI.create("https://testuser:testpass@www.acme.com/test/path?foo=bar#test-fragment");
+    URIBuilder builder = URIBuilder.of(uri);
+
+    assertThat(builder.getScheme()).isEqualTo("https");
+    assertThat(builder.getUserInfo()).isEqualTo("testuser:testpass");
+    assertThat(builder.getHost()).isEqualTo("www.acme.com");
+    assertThat(builder.getAuthority()).isEqualTo("testuser:testpass@www.acme.com");
+    assertThat(builder.getPath()).isEqualTo("/test/path");
+    assertThat(builder.getQuery()).isEqualTo("foo=bar");
+    assertThat(builder.getFragment()).isEqualTo("test-fragment");
+
+    assertThat(builder.toURI()).isEqualTo(uri);
+  }
+}
